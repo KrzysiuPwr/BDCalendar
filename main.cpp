47ed9ceb95d934d1a9b0411794a0f9ec015bc811
Joasia Klasik <asklas@op.pl>
2023-10-01 19:27:49 +0200

    formater

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 
-int main(int, char**){
-    std::cout << "Hello, from BDCalendar!\n";
-int var = 0;
-
-for(int i = 0; i < 9; i++)
+int main(int, char **)
 {
-}
+    std::cout << "Hello, from BDCalendar!\n";
+    int var = 0;
 
+    for (int i = 0; i < 9; i++)
+    {
+
+        std::cout << " co to sssssssssdksofsokofgdg    dge    sdgdkvo kokokokok okoko kok lklk ok oklk p kzjadnajngjfgjdnjdngjsndkjffnjgsjgnfjnjnej  jg ejgj\r\n ";
+    }
+
+    return 0;
 }
