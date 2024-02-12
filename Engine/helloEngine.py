8f777e7bdd35e4a9b6d2ed553c3c0d359b540413
Joasia Klasik <asklas@op.pl>
2024-02-12 22:54:33 +0100

    engine investigation

---
+++ Engine/helloEngine.py
@@ -0,0 +1,10 @@
+# importing sys
+
+import sys
+ 
+# adding Folder_2 to the system path
+sys.path.insert(0, '/Users/asialalala/Programy/Python/BCDCalendar/BDCalendar/Data')
+
+from helloData import helloData
+
+helloData()
