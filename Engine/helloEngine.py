4e25ef1a2d92e3280f6dcbdbf5b58169816cd93b
Joasia Klasik <asklas@op.pl>
2024-02-14 21:07:56 +0100

    getting environmental variables

--- Engine/helloEngine.py
+++ Engine/helloEngine.py
@@ -1,9 +1,23 @@
 import sys
+import os
 
-sys.path.append('/home/krzysztof/Calendar/BDCalendar/GUI')
+# Get environmental variable 
+GUI_PATH = os.getenv('GUI_PATH')
+DATA_PATH = os.getenv('DATA_PATH')
+
+# Append path to required files
+sys.path.append(GUI_PATH)
+sys.path.append(DATA_PATH)
+
 from helloGUI import GUI
+from helloData import helloData
+
+
 def helloEngine():
   msg = "Engine unluckily sucks :("
   print(msg)
+
+
 GUI()
+helloData()
 helloEngine()
